54fae0cbda5d9f2d7c46cf8ad147e9d818fbb19d
jamesquietly <jamesquietly@gmail.com>
2017-09-09 20:02:17 -0700

    guess limiting

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -1,26 +1,43 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(101)
-
+$secret = rand(101)
+$remaining_guesses = 5
 def check_guess(guess)
     message = ""
-    if guess > SECRET_NUMBER
-        if guess > SECRET_NUMBER + 5
+    if guess > $secret
+        if guess > $secret + 5
             message = "Way too high!"
         else
             message = "Too high!"
         end
-    elsif guess < SECRET_NUMBER
-        if guess < SECRET_NUMBER - 5
+    elsif guess < $secret
+        if guess < $secret - 5
             message = "Way too low!"
         else
             message = "Too low!"
         end
     else
-        message = "You got it right!\nThe SECRET NUMBER is #{SECRET_NUMBER}"
+        message = "You got it right!\nThe SECRET NUMBER is #{$secret}, GENERATING NEW NUMBER!"
+        $remaining_guesses = 6
     end
+    $remaining_guesses -= 1
+    if $remaining_guesses == 0
+        message += " YOU LOST! GENERATING NEW NUMBER!"
+        $remaining_guesses = 5
+    end
+    message += " || Remaining Guesses: #{$remaining_guesses}"
     message
+end
+
+def update_guess(number)
+    if number == 1
+        result = 5
+        secret = rand(101)
+    else
+        result = number - 1
+    end
+    result
 end
 
 get '/' do
